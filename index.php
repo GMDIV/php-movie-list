fb6888ce4cbe4924205f04c85852ff771ee8de43
Mitchell Deane <mitchelld20@yahoo.com>
2017-02-02 03:37:03 -0600

    added links to each movie and director along with some css touch ups.

--- index.php
+++ index.php
@@ -8,10 +8,11 @@ $json = json_decode($jsondata, true);
 <html>
 <head>
 	<style>
+		body {background-color: #c2d6d6; }
 		h1 {text-align: center;}
-		h4 {margin: 0; padding: 5px; background: #f4f4f4;}
-		li {list-style: none; padding-left: 5px;}
-		#container {width: 600px; margin: auto; overflow: hidden;}
+		h4 {margin: 0; padding: 5px; background: #f9f2f5; padding-right: 5px;}
+		li {list-style: none; padding-left: 5px; padding-right: 5px;}
+		#container {width: 600px; margin: auto; overflow: hidden; }
 	</style>
 	<title>Movies!!!!</title>
 </head>
@@ -21,11 +22,11 @@ $json = json_decode($jsondata, true);
 		<ul>
 			<?php 
 				foreach($json['movies'] as $key  => $value){
-					echo '<h4 ><a href="$value['movieLink']">'.$value['title'].'</a></h4>';
+					echo '<h4 ><a href="'.$value['movieLink'].'">'.$value['title'].'</a></h4>';
 					echo '<li>Year: '.$value['year'].'</li>';
 					echo '<li>Genre: '.$value['genre'].'</li>';
 					echo '<li>Role: '.$value['role'].'</li>';
-					echo '<li>Director: '.$value['director'].'</li>'; 
+					echo '<li>Director: <a href="'.$value['directorLink'].'">'.$value['director'].'</a></l1>';
 				}
 			?>
 		</ul>
